b4c21c2fe8185b0dc3c87029bfea9830f37d8578
US4 <mangokirby89@gmail.com>
2024-10-17 19:46:56 -0400

    Create nav-apps.js

---
+++ nav-apps.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const navButtons = `
+        <a href="/games">🕹️ Games</a>
+        <a href="/partners">👥 Partners</a>
+        <a href="/apps" style="color: gold; font-weight: bold;">📙 Apps & More</a>
+        <a href="/settings">⚙️ Settings</a>
+        <a href="/extra">➕ Extra</a>
+    `;
+
+    document.querySelector('.nav-buttons').innerHTML = navButtons;
+});
