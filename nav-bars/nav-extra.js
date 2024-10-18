3b1a7663bdf0909d1e73276dea33f35c45f2d378
US4 <mangokirby89@gmail.com>
2024-10-18 15:37:05 -0400

    Update nav-extra.js

--- nav-bars/nav-extra.js
+++ nav-bars/nav-extra.js
@@ -2,9 +2,9 @@ document.addEventListener("DOMContentLoaded", function () {
     const navButtons = `
         <a href="/games">🕹️ Games</a>
         <a href="/partners">👥 Partners</a>
-        <a href="/apps" style="color: gold; font-weight: bold;">📙 Apps & More</a>
+        <a href="/apps">📙 Apps & More</a>
         <a href="/settings">⚙️ Settings</a>
-        <a href="/extra">➕ Extra</a>
+        <a href="/extra" style="color: gold; font-weight: bold;">➕ Extra</a>
     `;
 
     document.querySelector('.nav-buttons').innerHTML = navButtons;
