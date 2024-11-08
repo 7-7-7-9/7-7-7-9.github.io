645f59f4f65d740d59cf8a42d1060224b7918279
US4 <mangokirby89@gmail.com>
2024-11-08 17:45:35 -0500

    Update nav-apps.js

--- nav-bars/nav-apps.js
+++ nav-bars/nav-apps.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
     const navButtons = `
-        <a href="/games">🕹️ Games</a>
-        <a href="/partners">👥 Partners</a>
-        <a href="/apps" style="color: gold; font-weight: bold;">📙 Apps & More</a>
-        <a href="/settings">⚙️ Settings</a>
-        <a href="/extra">➕ Extra</a>
+    <a href="/games"><i class="fas fa-gamepad"></i> Games</a>
+    <a href="/partners"><i class="fa-solid fa-users"></i> Partners</a>
+    <a href="/apps"><i class="fas fa-border-all"></i> Apps & More</a>
+    <a href="/settings"><i class="fa-solid fa-gear"></i> Settings</a>
+    <a href="/extra"><i class="fas fa-plus"></i> Extra</a>
     `;
 
     document.querySelector('.nav-buttons').innerHTML = navButtons;
