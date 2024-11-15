227b2e1d1b017d179fd1ac0019337d5a7ffd26c7
US4 <mangokirby89@gmail.com>
2024-11-15 16:30:40 -0500

    Update nav-partners.js

--- nav-bars/nav-partners.js
+++ nav-bars/nav-partners.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
     <a href="/partners"><i class="fa-solid fa-users"></i> Partners</a>
     <a href="/apps"><i class="fas fa-border-all"></i> Apps & More</a>
     <a href="/prox"><i class="fas fa-globe"></i> Proxy</a>
-    <a href="/settings"><i class="fa-solid fa-gear"></i> Settings</a>
+    <a href="/settings/main"><i class="fa-solid fa-gear"></i> Settings</a>
     <a href="/extra"><i class="fas fa-plus"></i> Extra</a>
     `;
 
