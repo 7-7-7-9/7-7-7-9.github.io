27e5627ee2126f8a9e98b975807f2cbecff3c1f1
US4 <mangokirby89@gmail.com>
2024-11-15 21:38:09 -0500

    Update nav-apps.js

--- nav-bars/nav-apps.js
+++ nav-bars/nav-apps.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const navButtons = `
     <a href="/games/index"><i class="fas fa-gamepad"></i> Games</a>
     <a href="/movies/index"><i class="fa-solid fa-video"></i> Movies</a>
-    <a href="/apps/index"><i class="fas fa-border-all"></i> Apps & More</a>
+    <a href="/apps/index"><i class="fa-sharp fa-layer-group"></i> Apps & More</a>
     <a href="/prox/index"><i class="fas fa-globe"></i> Proxy</a>
     <a href="/settings/main"><i class="fa-solid fa-gear"></i> Settings</a>
     <a href="/partners/index"><i class="fa-solid fa-users"></i> Partners</a>
