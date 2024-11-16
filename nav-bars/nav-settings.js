f3826259d2f25f5a0ac8812be1814239860e9a41
US4 <mangokirby89@gmail.com>
2024-11-15 21:00:09 -0500

    Update nav-settings.js

--- nav-bars/nav-settings.js
+++ nav-bars/nav-settings.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
     const navButtons = `
-    <a href="/games"><i class="fas fa-gamepad"></i> Games</a>
-    <a href="/movies"><i class="fa-solid fa-video"></i> Movies</a>
-    <a href="/apps"><i class="fas fa-border-all"></i> Apps & More</a>
-    <a href="/prox"><i class="fas fa-globe"></i> Proxy</a>
+    <a href="/games/index"><i class="fas fa-gamepad"></i> Games</a>
+    <a href="/movies/index"><i class="fa-solid fa-video"></i> Movies</a>
+    <a href="/apps/index"><i class="fas fa-border-all"></i> Apps & More</a>
+    <a href="/prox/index"><i class="fas fa-globe"></i> Proxy</a>
     <a href="/settings/main"><i class="fa-solid fa-gear"></i> Settings</a>
-    <a href="/partners"><i class="fa-solid fa-users"></i> Partners</a>
-    <a href="/extra"><i class="fas fa-plus"></i> Extra</a>
+    <a href="/partners/index"><i class="fa-solid fa-users"></i> Partners</a>
+    <a href="/extra/index"><i class="fas fa-plus"></i> Extra</a>
     `;
 
     document.querySelector('.nav-buttons').innerHTML = navButtons;
