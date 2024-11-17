203db8b159455d6d5f1bd42e3eeb67d6fa8c1192
US4 <mangokirby89@gmail.com>
2024-11-16 19:13:16 -0500

    Update edit.js

--- prox/edit.js
+++ prox/edit.js
@@ -1,6 +1,6 @@
 (function() {
     var x = document.createElement("script");
-    x.src = "https://cdn.jsdelivr.net/gh/SnowLord7/devconsole@master/main.js";
+    x.src = "/prox/inspect.js";
     x.onload = alert("Loaded Developer Console!");
     document.head.appendChild(x);
 })()
