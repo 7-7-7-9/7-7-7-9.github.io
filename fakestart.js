529ee19ea2c0abd94ed08a32ab426cc055e47611
US4 <mangokirby89@gmail.com>
2024-12-06 20:45:47 -0500

    Update fakestart.js

--- fakestart.js
+++ fakestart.js
@@ -29,7 +29,7 @@ window.addEventListener("load", function() {
     loadingScreen.appendChild(text);
 
     const tipText = document.createElement("p");
-    tipText.textContent = "Jay C i hope both sites of you pillow are warm tonight. If you ever beat me in quizizz again i will find you 🙏 - US4 Developer, Voucan";
+    tipText.textContent = "Jay C, I hope both sides of your pillow are warm tonight 🙏 - US4 Developer, Voucan";
     tipText.style.fontSize = "18px";
     tipText.style.fontWeight = "normal";
     tipText.style.marginTop = "10px";
