bbdfe232472184da47e279484a462b684e25080f
US4 <mangokirby89@gmail.com>
2024-12-08 10:40:20 -0500

    Create title.js

---
+++ nav-bars/title.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const header = `
+            <img src="https://raw.githubusercontent.com/US4-UBG/US4-UBG.github.io/refs/heads/main/logo.png" alt="Logo">
+            <a href="/">US4</a>
+    `;
+
+    document.querySelector('.nav-buttons').innerHTML = header;
+});
