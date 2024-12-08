82c65553977cb27aa5ff24b3037f550f3bf14d50
US4 <mangokirby89@gmail.com>
2024-12-08 10:46:44 -0500

    Update title.js

--- nav-bars/title.js
+++ nav-bars/title.js
@@ -4,5 +4,5 @@ document.addEventListener("DOMContentLoaded", function () {
             <a href="/">US4</a>
     `;
 
-    document.querySelector('.nav-buttons').innerHTML = header;
+    document.querySelector('.header').innerHTML = header;
 });
