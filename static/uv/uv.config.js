678b26b7b172709ae2438a3fe64dffd2ed692dfd
US4 <mangokirby89@gmail.com>
2024-12-09 19:50:24 -0500

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,21 @@
+const beforeUnloadEnabled = localStorage.getItem('beforeUnloadEnabled') === 'true';
+if (beforeUnloadEnabled) {
 self.__uv$config = {
     prefix: '/static/load/',
     bare:'https://bare-sigma.yourmom.us.kg/',
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: '/static/uv/uv.handler.js',
+    bundle: '/static/uv/uv.bundle.js',
+    config: '/static/uv/uv.config.js',
+    sw: '/static/uv/uv.sw.js',
+};
+
+const beforeUnloadEnabled = localStorage.getItem('beforeUnloadEnabled') === 'false';
+if (beforeUnloadEnabled) {
+self.__uv$config = {
+    prefix: '/static/load/',
+    bare:'https://example.com/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
