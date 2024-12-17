ce6b0e67b98dd5223a45ea553eb64523145cdfc2
US4 <mangokirby89@gmail.com>
2024-12-16 19:18:46 -0500

    Update nav-settings.js

--- nav-bars/nav-settings.js
+++ nav-bars/nav-settings.js
@@ -3,9 +3,8 @@ document.addEventListener("DOMContentLoaded", function () {
     <a href="/games"><i class="fas fa-gamepad"></i> Games</a>
     <a href="/movies"><i class="fa-solid fa-video"></i> Movies</a>
     <a href="/apps"><i class="fa-sharp fa-solid fa-layer-group"></i> Apps & More</a>
-    <a href="/prox"><i class="fas fa-globe"></i> Prøxy</a>
+    <a href="/prox"><i class="fas fa-globe"></i> Prօxy</a>
     <a href="/settings/main"><i class="fa-solid fa-gear"></i> Settings</a>
-    <a href="/partners"><i class="fa-solid fa-users"></i> Partners</a>
     <a href="/extra"><i class="fas fa-plus"></i> Extra</a>
     `;
 
