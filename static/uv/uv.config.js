b6c500c7fd01a3d019b2e24478c0b42ebc71b208
US4 <mangokirby89@gmail.com>
2024-12-18 15:38:31 -0500

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/load/',
-    bare:'https://bare-sigma.yourmom.eu.org/',
+    bare:'https://bare-benrogo-us4.global.ssl.fastly.net/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
